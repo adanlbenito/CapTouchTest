4f642b555b06aead647c65413cb02147a2078665
adanlbenito <adanl.benito@gmail.com>
2018-11-28 18:19:57 +0000

    Added setting options to example

--- render.cpp
+++ render.cpp
@@ -3,6 +3,10 @@
 
 CapTouch touchSensor;
 
+int speedOpts[4] = {0, 1, 2, 3};
+int prescalerOpts[6] = {1, 2, 4, 8, 16, 32};
+int thresholdOpts[7] = {0, 10, 20, 30, 40, 50, 60};
+int bitResolution = 12;
 
 void loop(void*)
 {
@@ -20,7 +24,37 @@ void loop(void*)
 
 bool setup(BelaContext *context, void *userData)
 {
-	touchSensor.setup();
+	if(touchSensor.setup() != 0) {
+		fprintf(stderr, "Unable to initialise touch sensor\n");
+		return false;
+	}
+
+	int newSpeed = speedOpts[0];
+	if(touchSensor.setScanSettings(newSpeed) == 0) {
+		fprintf(stderr, "Scan speed set to %d.\n", newSpeed);
+	} else {
+		return false;
+	}
+
+	int newPrescaler = prescalerOpts[0];
+	if(touchSensor.setPrescaler(newPrescaler) == 0) {
+		fprintf(stderr, "Prescaler set to %d.\n", newPrescaler);
+	} else {
+		return false;
+	}
+
+	int newThreshold= thresholdOpts[1];
+	if(touchSensor.setNoiseThreshold(newThreshold) == 0) {
+		fprintf(stderr, "Threshold set to %d.\n", newThreshold);
+	} else {
+		return false;
+	}
+
+	if(touchSensor.updateBaseLine() != 0)
+		return false;
+
+	if(touchSensor.prepareForDataRead() != 0)
+		return false;
 	Bela_scheduleAuxiliaryTask(Bela_createAuxiliaryTask(loop, 50, "I2C-read", NULL));
 	return true;
 }
